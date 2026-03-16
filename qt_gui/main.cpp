366bb70f2d301afb9d87c27725b38da5e3f416ab
Andrey Gerasimov <81647354+FireGoose@users.noreply.github.com>
2026-03-16 11:50:21 +0300

    TUpdate main.cpp

--- qt_gui/main.cpp
+++ qt_gui/main.cpp
@@ -8,8 +8,6 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   app.setApplicationName("Polynomial Algebra System");
   app.setApplicationVersion("1.0");
-  app.setOrganizationName("NNSTU");
-  app.setOrganizationDomain("nnstu.ru");
   app.setStyle(QStyleFactory::create("Fusion"));
   MainWindow window;
   window.show();
